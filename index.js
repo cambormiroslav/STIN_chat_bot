a4e238d6331b181338082f50a0b4b409dddf20a4
miroslav.cambor <miroslav.cambor@tul.cz>
2022-04-30 11:46:25 +0200

    add new method and tests

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ app.use(express.json());
 
 //data
 const name = "Tony";
-const help = { "exchange_today/exchange_today eur": "exchange today rate Czech crowns to Euro", "help": "my functions", "name": "my name", "time": "time now" };
+const help = { "what is the course / What is the course / what is the course for EUR / What is the course for EUR": "exchange today rate Czech crowns to Euro", "help / Help": "my functions", "what is your name / What is your name": "my name", "what is the time / What is the time": "time now" };
 const can_be_find_exchange = { "eur": "EUR" };
 const exchange_output = { "eur": "Euro" };
+const start_day_date_string = date.format(new Date(), 'DD.MM.YYYY');
+//const start_day_date_string = "29.04.2022"; //for test only when server starts in weekend
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/bot.html');
@@ -72,7 +74,7 @@ app.post("/exchange_today", (req, res, next) => {
     const today = new Date();
     const string_today_date = date.format(today, 'DD.MM.YYYY');
     const from_date = new Date();
-    from_date.setDate(from_date.getDate() - 2);
+    from_date.setDate(from_date.getDate() - 6);
     const string_from_date = date.format(from_date, 'DD.MM.YYYY');
 
     let getExchange = 0;
@@ -105,6 +107,55 @@ app.post("/exchange_today", (req, res, next) => {
         let result = { "output": "You want some exchange, which I cannot done or you type two parameters." };
         res.json(result);
         res.send();
+    }
+});
+
+app.post("/history", (req, res, next) => {
+    const today = new Date();
+    const string_today_date = date.format(today, 'DD.MM.YYYY');
+
+    const tomorow = new Date();
+    tomorow.setDate(tomorow.getDate() + 1);
+    const string_tomorow_date = date.format(tomorow, 'DD.MM.YYYY');
+
+    if (start_day_date_string == string_today_date || start_day_date_string == string_tomorow_date && today.getDay() == (6 || 7)) {
+        let result = { "output": "It is weekend and I live from this weekend, I am sorry. Came back in Monday for first history." };
+        res.json(result);
+        res.send();
+    } else {
+        let getExchange = 0;
+        let exchange = "";
+        let exghange_printed = "";
+        if (req.body.first == undefined) {
+            exchange = can_be_find_exchange["eur"];
+            exghange_printed = exchange_output["eur"];
+            getExchange = 1;
+        } else if (can_be_find_exchange[req.body.first] != null && req.body.second == undefined) {
+            exchange = can_be_find_exchange[req.body.first];
+            exghange_printed = exchange_output[req.body.first];
+            getExchange = 1;
+        }
+
+        if (getExchange == 1) {
+            axios.get(`https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/vybrane.html?od=${start_day_date_string}&do=${string_today_date}&mena=${exchange}&format=html`).then(({ data }) => {
+                let output = "";
+                let dat = data;
+                let table_from_start = dat.split("<table>")[1];
+                let table_to_end = table_from_start.split("</table>")[0];
+                let rows = table_to_end.split("\n");
+                for (let i = 2; i < rows.length - 1; i++) {
+                    output = `${output}{${rows[i].split("</td>")[0].replace("<tr><td>", "")}: ${rows[i].split("</td>")[1].replace("<td>", "")}},`
+                }
+
+                let result = { "output": `Exchange history for ${exghange_printed} from start of server is ${output}.` };
+                res.json(result);
+                res.send();
+            });
+        } else {
+            let result = { "output": "You want some history for exchange, which I cannot done or you type more than one parameter." };
+            res.json(result);
+            res.send();
+        }
     }
 });
 
--- scripts/bot.js
+++ scripts/bot.js
@@ -1,6 +1,6 @@
 function init() {
     let res_elm = document.createElement("div");
-    res_elm.innerHTML = "Hello, how can I help you? If you haven't idea what I can do send me the command: help.";
+    res_elm.innerHTML = "Hello, how can I help you? If you haven't idea what I can do send me the command: help / Help.";
     res_elm.setAttribute("class", "left");
 
     document.getElementById('msg').appendChild(res_elm);
@@ -10,14 +10,31 @@ function init() {
 document.getElementById('reply').addEventListener("click", async(e) => {
     e.preventDefault();
 
-    var req = document.getElementById('msg_send').value;
+    let sended_message = document.getElementById('msg_send').value;
+
+    var req = "";
+
+    if (sended_message.match(/help/) || sended_message.match(/Help/)) {
+        req = "help";
+    } else if (sended_message.match(/What is your name/) || sended_message.match(/what is your name/)) {
+        req = "name";
+    } else if (sended_message.match(/What is the time/) || sended_message.match(/what is the time/)) {
+        req = "time";
+    } else if (sended_message.match(/What is the course/) || sended_message.match(/what is the course/)) {
+        req = "exchange_today";
+    } else if (sended_message.match(/What is the course for EUR/) || sended_message.match(/what is the course for EUR/)) {
+        req = "exchange_today eur";
+    } else if (sended_message.match(/What is the history/) || sended_message.match(/what is the history/)) {
+        req = "history";
+    } else if (sended_message.match(/What is the history for EUR/) || sended_message.match(/what is the history for EUR/)) {
+        req = "history eur";
+    }
+
+    var res = "";
 
     if (req == undefined || req == "") {
-
+        res = "I don't understand you. If you haven't idea what I can do. Send me the command: help / Help";
     } else {
-
-        var res = "";
-
         var requests = req.split(" ");
 
         var data;
@@ -55,39 +72,37 @@ document.getElementById('reply').addEventListener("click", async(e) => {
         } catch (error) {
             res = "This function isn't defined.";
         }
-
-        let data_req = document.createElement('div');
-        let data_res = document.createElement('div');
-
-        let container1 = document.createElement('div');
-        let container2 = document.createElement('div');
-
-        container1.setAttribute("class", "msgCon1");
-        container2.setAttribute("class", "msgCon2");
-
-        data_req.innerHTML = req;
-        data_res.innerHTML = res;
-
-
-        data_req.setAttribute("class", "right");
-        data_res.setAttribute("class", "left");
-
-        let message = document.getElementById('msg');
-
-
-        message.appendChild(container1);
-        message.appendChild(container2);
-
-        container1.appendChild(data_req);
-        container2.appendChild(data_res);
-
-        document.getElementById('msg_send').value = "";
-
-        function scroll() {
-            var scrollMsg = document.getElementById('msg')
-            scrollMsg.scrollTop = scrollMsg.scrollHeight;
-        }
-        scroll();
-
     }
+    let data_req = document.createElement('div');
+    let data_res = document.createElement('div');
+
+    let container1 = document.createElement('div');
+    let container2 = document.createElement('div');
+
+    container1.setAttribute("class", "msgCon1");
+    container2.setAttribute("class", "msgCon2");
+
+    data_req.innerHTML = sended_message;
+    data_res.innerHTML = res;
+
+
+    data_req.setAttribute("class", "right");
+    data_res.setAttribute("class", "left");
+
+    let message = document.getElementById('msg');
+
+
+    message.appendChild(container1);
+    message.appendChild(container2);
+
+    container1.appendChild(data_req);
+    container2.appendChild(data_res);
+
+    document.getElementById('msg_send').value = "";
+
+    function scroll() {
+        var scrollMsg = document.getElementById('msg')
+        scrollMsg.scrollTop = scrollMsg.scrollHeight;
+    }
+    scroll();
 });
